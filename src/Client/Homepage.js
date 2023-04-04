1c2644faa02d28a47e5e2d8627d6c0b4da17e9ba
Ben Goldfried <2goldenfried@gmail.com>
2023-04-04 14:03:43 -0400

    Initial Commit

---
+++ src/Client/Homepage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Homepage(){
+
+const[test,setTest] = useState(0);
+    
+return(
+        <div>
+            Test!
+        </div>
+    );
+}
